b550f297b5efcfa691f54b8a450d5d37da109d3e
damiro <hello.damiro@gmail.com>
2023-05-11 13:30:29 +0800

    scenes css layout adjusted for real image

--- src/data/Scenes.js
+++ src/data/Scenes.js
@@ -2,16 +2,21 @@ export const scenesData = [
     {
         id: 1,
         name: 'Garage',
-        img: 'https://picsum.photos/200/400',
+        img: 'https://github.com/hello-damiro/odins-gaze/blob/main/src/assets/scene_garage.webp?raw=true',
     },
     {
         id: 2,
         name: 'Market',
-        img: 'https://picsum.photos/200/400',
+        img: 'https://github.com/hello-damiro/odins-gaze/blob/main/src/assets/scene_market.webp?raw=true',
     },
     {
         id: 3,
+        name: 'Office',
+        img: 'https://github.com/hello-damiro/odins-gaze/blob/main/src/assets/scene_office.webp?raw=true',
+    },
+    {
+        id: 4,
         name: 'Yard',
-        img: 'https://picsum.photos/200/400',
+        img: 'https://github.com/hello-damiro/odins-gaze/blob/main/src/assets/scene_yard.webp?raw=true',
     },
 ];
